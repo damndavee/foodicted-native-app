1173f3f197f021adc1dc43736a0458ba19741b9a
Pawel Kopij <pawelrd@gmail.com>
2024-01-31 13:31:29 +0100

    add signin and signup background images

--- App.tsx
+++ App.tsx
@@ -5,22 +5,26 @@ import { NativeBaseProvider } from 'native-base';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SplashScreen from "./src/screens/Splash";
 import useImageLoader from './src/hooks/useImageLoader';
+import AuthScreen from './src/screens/main/Auth';
 
 export default function App() {
 
   const [imagesLoaded] = useImageLoader([
     require('./assets/splash-icon.png'),
     require('./assets/welcome-screen.jpg'),
+    require('./assets/signin-screen.jpg'),
+    require('./assets/signup-screen.jpg'),
   ]);
 
   return (
       <SplashScreen isLoaded={imagesLoaded!}>
         <SafeAreaProvider>
           <NativeBaseProvider>
-            <View style={styles.container}>
               <StatusBar style="auto" />
+              <AuthScreen />
+            {/* <View style={styles.container}>
               <Text>Open up App.tsx to start working on your app!</Text>
-            </View>
+            </View> */}
           </NativeBaseProvider>
         </SafeAreaProvider>
       </SplashScreen>
